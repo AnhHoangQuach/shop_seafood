f1ba1d3f36a9c818a7c76f34d41820a81df85c6a
AnhHoangQuach <hoanganh36.work@gmail.com>
2020-07-28 21:26:47 +0700

    create header fe

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://kit.fontawesome.com/d8b310bdda.js" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col-md-2 columns_wrap">
+                    <span class="icon_left"><i class="fas fa-map-marker-alt"></i></span>
+                    <span class="contact_label"> 36 Ngu Hanh <br> Son, Da Nang</span>
+                </div>
+                <div class="col-md-2 columns_wrap">
+                    <span class="icon_left"><i class="fas fa-phone-volume"></i></span>    
+                    <span class="contact_label">Call us at <br> <a href="tel:0869679324"> 0869679324</a></span>
+                </div>
+                <div class="col-md-4 columns_wrap">
+                    <a href="index.php"><img src="images/logo.png" alt=""></a>
+                </div>
+                <ul class="col-md-2 columns_wrap">
+                    <li>
+                        <span class="icon_right"><i class="fas fa-user"></i></span>  
+                    </li>
+                    <li class="user_login">
+                        <span class="contact_label_right">
+                            <a id="cart-popover" data-placement="bottom" title>Login</a>
+                        </a>
+                    </li>
+                </ul>
+                <div class="col-md-2 columns_wrap">
+                    <span class="icon-right"><i class="fas fa-shopping-cart"></i></span>    
+                    <span class="contact_label_right">$0.00</span>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
