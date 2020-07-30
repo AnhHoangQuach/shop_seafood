ddb47b69d4fdbbc2d80cac007293e4976813b7be
AnhHoangQuach <hoanganh36.work@gmail.com>
2020-07-30 21:15:27 +0700

    create product_feature

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Seafood Store</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/d8b310bdda.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
 </head>
@@ -78,20 +79,51 @@
             <div class="item">
                 <img class="img-responsive" src="images/slides/slides_1.jpg" alt="">
                 <div class="item-caption">
-                    <h1 class="caption">THE WONDERS OF THE OCEAN</h1>
-                    <h1 class="caption">ON THE PLATE</h1>
+                    <h1 class="caption animate__animated animate__bounce">THE WONDERS OF THE OCEAN</h1>
+                    <h4 class="caption_bottom animate__animated animate__fadeInLeft">ON THE PLATE</h4>
                 </div>
             </div>
             <div class="item">
                 <img src="images/slides/slides_2.jpg" alt="">
+                <div class="item-caption">
+                    <h1>OCEAN VARIETY, POND PRICE</h1>
+                    <h4 class="caption_bottom">IN OUR STORE</h4>
+                </div>
             </div>
             <div class="item">
                 <img src="images/slides/slides_3.jpg" alt="">
+                <div class="item-caption">
+                    <h1>THE PLACE TO BE FOR SEAFOOD</h1>
+                    <h4 class="caption_bottom">AT THE RESTAURANT</h4>
+                </div>
             </div>
         </div>
     </section>
     <div class="page_content_wrap">
-        <h1>Hello</h1>
+        <div class="container">
+            <div class="empty_space"></div>
+            <div class="content_wrap">
+                <div class="product_feature">
+                    <h2>LEADER</h2>
+                    <span>Seafood</span>
+                </div>
+                <div class="product_feature">
+                    <h2>PARTNER</h2>
+                    <span>Sustainable</span>
+                </div>
+                <div class="product_feature">
+                    <h2>PRODUCT</h2>
+                    <span>World Class</span>
+                </div>
+                <div class="product_feature">
+                    <h2>QUALITY</h2>
+                    <span>Control</span>
+                </div>
+            </div>
+            <div class="section_introduce">
+                <p>Welcome</p>
+            </div>
+        </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.js" 
     integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    nav:false,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    responsive:{
-        0:{
-            items:1,
-        },
-    }
-});
-// putting dots to slide
-var dots = $('.owl-dots').css('position', 'absolute').css('bottom', '5px');
-dots.css('left', 'calc(50% - ' + dots.width()/2+'px)');
-
 $(document).ready(function() {
     $(window).scroll(function() {
         var headerHeight = $('.header_top').height();
@@ -23,18 +7,65 @@ $(document).ready(function() {
             $('.header_bottom').removeClass('navbar_fixed');
         }
     });
+    var heroSlider = $('.owl-carousel');
+    heroSlider.owlCarousel({
+        loop:true,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+        }
+    });
+    // putting dots to slide
+    var dots = $('.owl-dots').css('position', 'absolute').css('bottom', '5px');
+    dots.css('left', 'calc(50% - ' + dots.width()/2+'px)');
+    heroSlider.on('changed.owl.carousel', function(event) {
+        var item = event.item.index-2;
+        $('h1').removeClass('animate__animated animate__bounce');
+        $('.owl-item').not('.cloned').eq(item).find('h1').addClass('animate__animated animate__bounce');
+        $('h4').removeClass('animate__animated animate__fadeInLeft');
+        $('.owl-item').not('.cloned').eq(item).find('h4').addClass('animate__animated animate__fadeInLeft');
+    });
+
+    function hasClass(element, cls) {
+        return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+    }
+
+    var product_feature = $('.product_feature');
+    var product_len = product_feature.length;
+    var product_count = 1;
+    if(product_count !== product_len) {
+        product_feature.each(function() {
+            var imgSrc = "../shop_seafood/images/bg_feature/bg_feature" + `${product_count++}` + ".jpg";
+            $(this).css('background-image', 'url("' + imgSrc + '")');
+            $(this).addClass('product_feature_all');
+            if(product_count > 2) {
+                $(this).addClass('product_margin');
+            }
+        });
+    }
 });
 
-// split text
-// const text = document.querySelector('.caption');
-// const strText = text.textContent;
-// const splitText = strText.split("");
-// text.textContent = "";
-// for(let i=0;i<splitText.length;i++) {
-//     text.innerHTML += "<span>" + splitText[i] + "</span>";
+//split text
+// var text;
+// var char;
+// var timer;
+// var splitText;
+// function split_string(className) {
+//     text = document.querySelector(`${className}`);
+//     const strText = text.textContent;
+//     splitText = strText.split("");
+//     text.textContent = "";
+//     for(let i=0;i<splitText.length;i++) {
+//         text.innerHTML += "<span>" + splitText[i] + "</span>";
+//     }
+//     char = 0;
+//     timer = setInterval(onTick,50);
 // }
-// let char = 0;
-// let timer = setInterval(onTick,50);
 
 // function onTick() {
 //     const span = text.querySelectorAll('span')[char];
@@ -50,29 +81,4 @@ $(document).ready(function() {
 //     clearInterval(timer);
 //     timer = null;
 // }
-var char = 0;
-const text = document.querySelectorAll('.caption');
-for(var i = 0;i < Math.floor(text.length/2); i++) {
-    var strText = text[i].textContent;
-    var splitText = strText.split("");
-    text[i].textContent = "";
-    for(var j = 0; j < Math.floor(splitText.length); j++) {
-        text[i].innerHTML += "<span>" + splitText[j] + "</span>";
-    }
-    let timer = setInterval(onTick(text[i]),35);
-}
-
-function onTick(text) {
-    const span = text.querySelectorAll('span')[char];
-    span.classList.add('fade');
-    char++;
-    if(char === splitText.length) {
-        complete();
-        return;
-    }
-}
-
-function complete() {
-    clearInterval(timer);
-    timer = null;
-}
+// split_string('.caption');
